f396f3a642a975916913c258c9399bede9458f60
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-12-18 16:29:56 +0200

    Create Renderer.java

---
+++ src/com/company/Renderer.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.time.LocalTime;
+
+public interface Renderer {
+    void initialize();
+    void shutdown();
+
+    MenuEntry renderMenu(boolean isOnline);
+    void renderPlayField(PlayField playField);
+    TurnInformation askGameTurn(Player currentPlayer);
+    void drawMessage(String text);
+    int askPlayFieldSize();
+    void drawGameFinished(PlayField playField, LocalTime startTime, LocalTime endTime, Player winner);
+}
