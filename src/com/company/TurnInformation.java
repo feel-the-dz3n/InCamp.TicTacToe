d9669ad16a62bcf9e012dbab330d0552f5634216
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-12-18 16:30:08 +0200

    Create TurnInformation.java

---
+++ src/com/company/TurnInformation.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class TurnInformation {
+    private int row;
+    private int column;
+
+    public TurnInformation(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+}
